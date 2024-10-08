671ea34d770a50f4fe6be4893bec797573c5eebe
Sam Verdon <sam.verdon@transigma.com>
2024-10-08 23:48:05 +0100

    fixed

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
-import "math/rand"
+import (
+	"log"
+	"math/rand"
+	"sync/atomic"
+)
+
+const totalSimulations uint64 = 1_000_000
+
+var completedSimulations uint64
 
 func roll() int {
 	var count int
 	for i := 1; i <= 231; i++ {
-		if rand.Float32() <= 0.25 {
+		if rand.Intn(4) == 0 {
 			count++
 		}
 	}
+	atomic.AddUint64(&completedSimulations, 1)
 	return count
 }
 
@@ -19,29 +28,32 @@ func worker(jobs <-chan int, results chan<- int) {
 }
 
 func main() {
-	const pool = 25_000
-	const upper = 1_000_000_000
-	rolls := upper / pool
+	const pool = 12
+	const upper = int(totalSimulations)
 
-	jobs := make(chan int, rolls)
-	results := make(chan int, rolls)
+	jobs := make(chan int, upper)
+	results := make(chan int, upper)
 
 	for w := 1; w <= pool; w++ {
 		go worker(jobs, results)
 	}
 
-	for j := 1; j <= rolls; j++ {
+	for j := 1; j <= upper; j++ {
 		jobs <- j
 	}
 	close(jobs)
 
 	var max int
-	for n := 1; n <= rolls; n++ {
+	for n := 1; n <= upper; n++ {
 		output := <-results
 		if output > max {
 			max = output
 		}
 	}
 
-	println(max)
+	if completedSimulations != totalSimulations {
+		log.Fatalf("Expected %d simulations but instead only %d were performed", totalSimulations, completedSimulations)
+	} else {
+		println(max)
+	}
 }
